d6bb5e2e682f29692c87dcb07c8d7132e13323ee
Dardo Camaño <dardo@debianK52J.laboratorio.redes.edu.ar>
2021-09-23 16:25:58 -0300

    g

--- php/Conexion.php
+++ php/Conexion.php
@@ -10,20 +10,22 @@ URI postgres://vbplczzecsknma:a66f85ec92308f14d29b01ea8dc9198907de08134cd5c8d3db
 Heroku CLI heroku pg:psql postgresql-concave-17821 --app eest3necochea-school
 */
 
-$host     = "ec2-52-7-159-155.compute-1.amazonaws.com";
-$database = "d45iq9tlomq3kv";
-$user     = "vbplczzecsknma";
-$port     = "5432";
-$passw    = "a66f85ec92308f14d29b01ea8dc9198907de08134cd5c8d3db2b2753f22896ff";
+// $host     = "ec2-52-7-159-155.compute-1.amazonaws.com";
+// $database = "d45iq9tlomq3kv";
+// $user     = "vbplczzecsknma";
+// $port     = "5432";
+// $passw    = "a66f85ec92308f14d29b01ea8dc9198907de08134cd5c8d3db2b2753f22896ff";
+//
+// $conn_string  = "host=".$host." port=".$port." dbname=".$database." user=".$user." password=".$passw."";
+// $dbconn       = pg_connect($conn_string);
+//
+// //conectarse por el puerto 5432 a una base de datos llamada "test" en el
+// //host "sheep" con el nombre de usuario y password
+//
+// $result = pg_query($db_connection, "SELECT * FROM comments");
+//
+// var_dump($result);
 
-$conn_string  = "host=".$host." port=".$port." dbname=".$database." user=".$user." password=".$passw."";
-$dbconn       = pg_connect($conn_string);
-
-//conectarse por el puerto 5432 a una base de datos llamada "test" en el
-//host "sheep" con el nombre de usuario y password
-
-$result = pg_query($db_connection, "SELECT * FROM comments");
-
-var_dump($result);
+echo "string";
 
 ?>
