e0f5be1cc299cd7d7e006614f0dc169f1d076a9b
Dardo Camaño <dardo@debianK52J.laboratorio.redes.edu.ar>
2021-09-23 10:11:05 -0300

    ----

--- php/configuracion.php
+++ php/configuracion.php
@@ -1,6 +1,9 @@
 <?php
 define('HOME_URL', "https://eest3necochea-school.herokuapp.com/");
 
+//configuracion de la base de cargarDatos
+define('')
+
 if (!isset($_SESSION)) {
   session_name('school');
   session_set_cookie_params(0);
