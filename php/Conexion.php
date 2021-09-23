4c1e2f3455acca30124aa8e99d6bc8019997a169
Dardo Camaño <dardo@debianK52J.laboratorio.redes.edu.ar>
2021-09-23 16:40:22 -0300

    from script

--- php/Conexion.php
+++ php/Conexion.php
@@ -10,21 +10,26 @@ URI postgres://vbplczzecsknma:a66f85ec92308f14d29b01ea8dc9198907de08134cd5c8d3db
 Heroku CLI heroku pg:psql postgresql-concave-17821 --app eest3necochea-school
 */
 
-// $host     = "ec2-52-7-159-155.compute-1.amazonaws.com";
-// $database = "d45iq9tlomq3kv";
-// $user     = "vbplczzecsknma";
-// $port     = "5432";
-// $passw    = "a66f85ec92308f14d29b01ea8dc9198907de08134cd5c8d3db2b2753f22896ff";
-//
-// $conn_string  = "host=".$host." port=".$port." dbname=".$database." user=".$user." password=".$passw."";
-// $dbconn       = pg_connect($conn_string);
-//
-// //conectarse por el puerto 5432 a una base de datos llamada "test" en el
-// //host "sheep" con el nombre de usuario y password
-//
-// $result = pg_query($db_connection, "SELECT * FROM comments");
-//
-// var_dump($result);
+$host     = "ec2-52-7-159-155.compute-1.amazonaws.com";
+$database = "d45iq9tlomq3kv";
+$user     = "vbplczzecsknma";
+$port     = "5432";
+$passw    = "a66f85ec92308f14d29b01ea8dc9198907de08134cd5c8d3db2b2753f22896ff";
+
+$conn_string  = "host=".$host." port=".$port." dbname=".$database." user=".$user." password=".$passw;
+$dbconn       = pg_connect($conn_string);
+
+//conectarse por el puerto 5432 a una base de datos llamada "test" en el
+//host "sheep" con el nombre de usuario y password
+if ($dbconn) {
+  $result = pg_query($dbconn, "SELECT * FROM comments");
+  var_dump($result);
+}else {
+  echo "Error al conectar con la base de datos";
+}
+
+
+
 
 echo "string";
 
